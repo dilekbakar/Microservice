74afe15284d2b7f7167fcc446cafdcf3a4ef4a8f
Dilek BAKAR <108486801+dilekbakar@users.noreply.github.com>
2023-04-13 17:23:48 +0300

    common library added

---
+++ src/Common/Common.Library/Infrastructure/Extensions/PagingExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Common.Library.Models.Page;
+using Microsoft.EntityFrameworkCore;
+
+namespace Common.Library.Infrastructure.Extensions
+{
+    public static class PagingExtensions
+    {
+        public static PagedViewModel<T> GetPaged<T>(this IQueryable<T> query, int currentPage, int pageSize) where T : class
+        {
+            var count = query.Count();
+
+            Page paging = new(currentPage, pageSize, count);
+
+            var data = query.Skip(paging.Skip).Take(paging.PageSize).AsNoTracking().ToList();
+
+            var result = new PagedViewModel<T>(data, paging);
+
+            return result;
+        }
+    }
+}
---
+++ src/Common/Common.Library/Models/Page/BasePagedQuery.cs
@@ -0,0 +1,14 @@
+﻿namespace Common.Library.Models.Page
+{
+    public class BasePagedQuery
+    {
+        public BasePagedQuery(int page, int pageSize)
+        {
+            Page = page;
+            PageSize = pageSize;
+        }
+
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+    }
+}
