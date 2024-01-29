dea00ac153928e26deec2da5fc85158be27c644b
DİLEK BAKAR <dilek_bakar98@hotmail.com>
2024-01-29 11:41:08 +0300

    #web service created

---
+++ src/Services/WebService/WebService.Domain/Entities/Base/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace WebService.Domain.Entities.Base
+{
+    public abstract class BaseEntity : IEntity
+    {
+        public virtual long Id { get; set; }
+        public bool IsActive { get; set; } = true;
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public long CreatedUser { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public long? UpdatedUser { get; set; }
+        public int RowStatus { get; set; } = 1;
+    }
+}
---
+++ src/Services/WebService/WebService.Domain/Entities/Base/IEntity.cs
@@ -0,0 +1,20 @@
+﻿namespace WebService.Domain.Entities.Base
+{
+    public interface IEntity<TKey>
+        {
+            public TKey Id { get; set; }
+            public bool IsActive { get; set; }
+            public bool IsDeleted { get; set; }
+            public DateTime CreatedDate { get; set; }
+            public long CreatedUser { get; set; }
+            public DateTime? UpdatedDate { get; set; }
+            public long? UpdatedUser { get; set; }
+            public int RowStatus { get; set; }
+
+        }
+        public interface IEntity : IEntity<long>
+        {
+
+        }
+    
+}
